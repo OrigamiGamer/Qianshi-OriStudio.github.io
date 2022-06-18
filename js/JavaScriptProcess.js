15195f09307a0cb6aa44bc978242dd8afd031fb7
千矢 - Creakler <77380048+Qianshi-OriStudio@users.noreply.github.com>
2022-06-19 02:46:43 +0800

    Update EX!

--- js/JavaScriptProcess.js
+++ js/JavaScriptProcess.js
@@ -1,6 +1,14 @@
-function GetFaviconIcon(URL){
-	var xhr = XMLHttpRequest()
-	xhr.open('GET','http://statics.dnspod.cn/proxy_favicon/_/favicon?domain='+URL,true);
-	xhr.send(null);
-	return(xhr.responseText)
+function HtmlUrlRead(URL) {
+	var xhr = new XMLHttpRequest()
+	xhr.open('Get',URL,false)
+	xhr.send(null)
+	return (xhr.responseText)
+}
+function OutPutAboutMe() {
+	console.log('# 千矢 - Creakler')
+	console.log('# Bili: https://space.bilibili.com/439946965')
+	console.log('# QQ: 2937396379 非诚勿扰')
+	console.log('# 是 OrigamiStudio 的一员')
+	console.log('# 正在开发的有: ')
+	console.log('EzUpdate GitHub->https://github.com/Qianshi-OriStudio/EzUpdate')
 }
